a7b2dbb30db5fe1452a798542a6fc370b0947910
Ajay Poshak <poshakajay@gmail.com>
2023-05-22 20:32:46 +0530

    Refactor storage reading

---
+++ src/S3.js
@@ -0,0 +1,29 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { REGION } from "./constants.js";
+
+export class S3 {
+  constructor() {
+    this.s3Client = new S3Client({
+      region: REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+  }
+
+  async getPartialFile({ bucket, key, range }) {
+    if (range.start === undefined)
+      throw new Error("start key missing in range");
+    if (range.end === undefined) throw new Error("end key missing in range");
+    const { start, end } = range;
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: key,
+      Range: `bytes=${start}-${end}`,
+    });
+    console.log("=======> range ", { start, end });
+    const obj = await this.s3Client.send(command);
+    return obj;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createServer } from "node:http";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { parseValueFromString } from "./utils.js";
 import { REGION, HEADER_LENGTH } from "./constants.js";
+import { S3 } from "./S3.js";
+
+const storageClient = new S3();
 
 let INDEX_LENGTH = null,
   INDEX_START = null,
@@ -19,24 +22,13 @@ function capitalize(str) {
 async function readKeyFromData(keyName, dataRange) {
   console.log({ keyName, dataRange });
   const [dataStartIndex, keyLength] = dataRange;
-  const client = new S3Client({
-    region: REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    },
-  });
   const startIndex = HEADER_LENGTH + INDEX_LENGTH + dataStartIndex;
   const endIndex = startIndex + keyLength + 1;
-  console.log({ INDEX_LENGTH, HEADER_LENGTH, dataStartIndex, keyLength });
-  console.log(`=========> querying for range ${startIndex} - ${endIndex}`);
-
-  const command = new GetObjectCommand({
-    Bucket: process.env.BUCKET,
-    Key: "data_v3.txt",
-    Range: `bytes=${startIndex}-${endIndex}`,
+  const obj = await storageClient.getPartialFile({
+    bucket: process.env.BUCKET,
+    key: "data_v3.txt",
+    range: { start: startIndex, end: endIndex },
   });
-  const obj = await client.send(command);
   const { Body } = obj;
   let chunks = [];
   for await (const chunk of Body) {
@@ -71,19 +63,11 @@ const server = createServer(async (request, response) => {
 server.listen(4000);
 
 async function readIndexFromS3(start, length) {
-  const client = new S3Client({
-    region: REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    },
+  const obj = await storageClient.getPartialFile({
+    bucket: process.env.BUCKET,
+    key: "data_v3.txt",
+    range: { start: start, end: start + length - 1 },
   });
-  const command = new GetObjectCommand({
-    Bucket: process.env.BUCKET,
-    Key: "data_v3.txt",
-    Range: `bytes=${start}-${start + length - 1}`,
-  });
-  const obj = await client.send(command);
   const { Body } = obj;
   let chunks = [];
   for await (const chunk of Body) {
@@ -95,28 +79,22 @@ async function readIndexFromS3(start, length) {
 }
 
 async function readIndexIntoMemory() {
-  const client = new S3Client({
-    region: REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    },
+  const obj = await storageClient.getPartialFile({
+    bucket: process.env.BUCKET,
+    key: "data_v3.txt",
+    range: { start: 0, end: HEADER_LENGTH - 1 },
   });
-  const command = new GetObjectCommand({
-    Bucket: process.env.BUCKET,
-    Key: "data_v3.txt",
-    Range: `bytes=0-${HEADER_LENGTH - 1}`,
-  });
-  const obj = await client.send(command);
   const { Body } = obj;
   let chunks = [];
   for await (const chunk of Body) {
     chunks.push(chunk);
   }
   const header = chunks.join("");
+  console.log("========> ", header);
   INDEX_START = parseInt(parseValueFromString(header, "index_start"), 10);
   INDEX_LENGTH = parseInt(parseValueFromString(header, "index_length"), 10);
   wordIndex = await readIndexFromS3(INDEX_START, INDEX_LENGTH);
+  console.log("Index reading finished");
 }
 
 readIndexIntoMemory();
